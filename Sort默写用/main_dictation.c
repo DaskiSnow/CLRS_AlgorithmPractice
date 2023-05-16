3e5633c7a12fbc509a79d7264e40cf7521fbe545
DaskiSnow <437207609@qq.com>
2023-05-16 21:52:35 +0800

    测试一次VS的提交

--- Sort默写用/main_dictation.c
+++ Sort默写用/main_dictation.c
@@ -2,7 +2,7 @@
 #define SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 int main(void) {
-	int arr[] = { 1,3,5,34,34,13,54,88,952,100 };
+	int arr[] = { 1,3,5,34,34,13,54,88,952,100,200 };
 	//direct_insertion_sort(arr, SIZE(arr));
 	//select_sort(arr,0,SIZE(arr)-1);
 	//heap_sort(arr, SIZE(arr));
